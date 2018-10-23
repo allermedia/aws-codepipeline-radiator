bbca5afad628783285066c2b9c8d032e2ddbcc53
henrikra <henrik.raitasola@gmail.com>
2018-10-23 10:44:11 +0300

    render stage states and action states

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,25 @@ if (process.env.REACT_APP_ACCESS_KEY_ID && process.env.REACT_APP_SECRET_ACCESS_K
   console.error('Add REACT_APP_ACCESS_KEY_ID and REACT_APP_SECRET_ACCESS_KEY to .env');
 }
 
-class App extends React.Component {
+interface State {
+  codePipeline?: CodePipeline.GetPipelineStateOutput;
+  isLoading: boolean;
+}
+
+class App extends React.Component<{}, State> {
+  public state: Readonly<State> = { isLoading: false };
   public componentDidMount() {
     if (process.env.REACT_APP_CODE_PIPELINE_NAME) {
+      this.setState({ isLoading: true });
       new CodePipeline().getPipelineState(
         { name: process.env.REACT_APP_CODE_PIPELINE_NAME },
         (error, data) => {
           if (error) {
+            this.setState({ isLoading: false });
             console.log(error, error.stack);
           } else {
-            console.log(data);
+            console.log(data.stageStates);
+            this.setState({ codePipeline: data, isLoading: false });
           }
         },
       );
@@ -41,9 +50,34 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {this.state.isLoading && <p>Loading</p>}
+        {this.state.codePipeline && (
+          <>
+            <p className="App-intro">{this.state.codePipeline.pipelineName}</p>
+            <ol className="stageStates">
+              {this.state.codePipeline.stageStates &&
+                this.state.codePipeline.stageStates.map(stageState => {
+                  return (
+                    <li key={stageState.stageName}>
+                      {stageState.stageName} -{' '}
+                      {stageState.latestExecution && stageState.latestExecution.status}
+                      <ol className="actionStates">
+                        {stageState.actionStates &&
+                          stageState.actionStates.map(actionState => {
+                            return (
+                              <li key={actionState.actionName}>
+                                {actionState.actionName} -{' '}
+                                {actionState.latestExecution && actionState.latestExecution.status}
+                              </li>
+                            );
+                          })}
+                      </ol>
+                    </li>
+                  );
+                })}
+            </ol>
+          </>
+        )}
       </div>
     );
   }
