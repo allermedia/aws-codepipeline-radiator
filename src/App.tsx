c3445f5398dbfac65ad0084f692be9f2bb223cc2
henrikra <henrik.raitasola@gmail.com>
2018-10-23 10:18:01 +0300

    Add simple way to fetch pipeline data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
+import AWS, { CodePipeline } from 'aws-sdk';
 import * as React from 'react';
 import './App.css';
 
 import logo from './logo.svg';
 
+if (process.env.REACT_APP_ACCESS_KEY_ID && process.env.REACT_APP_SECRET_ACCESS_KEY) {
+  AWS.config.update({
+    credentials: {
+      accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
+      secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
+    },
+    region: process.env.REACT_APP_REGION,
+  });
+} else {
+  console.error('Add REACT_APP_ACCESS_KEY_ID and REACT_APP_SECRET_ACCESS_KEY to .env');
+}
+
 class App extends React.Component {
+  public componentDidMount() {
+    if (process.env.REACT_APP_CODE_PIPELINE_NAME) {
+      new CodePipeline().getPipelineState(
+        { name: process.env.REACT_APP_CODE_PIPELINE_NAME },
+        (error, data) => {
+          if (error) {
+            console.log(error, error.stack);
+          } else {
+            console.log(data);
+          }
+        },
+      );
+    } else {
+      console.error('Add REACT_APP_CODE_PIPELINE_NAME to .env');
+    }
+  }
+
   public render() {
     return (
       <div className="App">
