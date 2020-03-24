cfdc1a8e6ea2cd03bb430392cd47b9833381bed6
Mikko Tikkanen <mikko.tikkanen@aller.com>
2020-03-24 15:12:42 +0200

    Added more error checing

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,13 +7,21 @@ const router = Router();
 const getPipelineState = (name: string) => codepipeline.getPipelineState({ name }).promise();
 
 router.get('/api/codepipelines/state', (req, res) => {
-  const codePipelineNames = req.query.q.split(',');
-  Promise.all(codePipelineNames.map(getPipelineState))
-    .then(data => res.json(data))
-    .catch(err => {
-      console.log('Error:', err);
-      res.status(500);
-    });
+  if (!req.query.q) {
+    res.status(400).json({
+      message: 'Query (q=) param is required',
+    })
+  } else {
+    const codePipelineNames = req.query.q.split(',');
+    Promise.all(codePipelineNames.map(getPipelineState))
+      .then(data => res.json(data))
+      .catch(err => {
+        console.log('Error:', err);
+        res.status(500).json({
+          message: err.message,
+        });
+      });
+  }
 });
 
 export default router;
