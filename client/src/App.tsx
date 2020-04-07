85f9809ba40ca29ae427d602999abfee4e25dba5
Mikko Tikkanen <mikko.tikkanen@aller.com>
2020-04-07 11:44:38 +0300

    eslint fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { CodePipeline } from 'aws-sdk';
 import classNames from 'classnames';
-import * as React from 'react';
+import React, { ReactNode } from 'react';
 import './App.css';
 import axios from 'axios';
 
@@ -18,19 +18,23 @@ type State = {
 
 class App extends React.Component<Props, State> {
   public state: Readonly<State> = { isLoading: false, showEnvFileError: false };
-  public componentDidMount() {
+
+  public componentDidMount(): void {
     this.fetchApi();
     setInterval(this.fetchApi, 5000);
   }
 
-  public fetchApi = () => {
+  public fetchApi = (): void => {
     this.setState({ isLoading: true });
 
     const queryParam = new URLSearchParams(window.location.search).get('q');
     if (queryParam) {
       axios.get(`/api/codepipelines/state?q=${queryParam}`)
         .then((res) => {
-          this.setState({ isLoading: false, codePipelines: res.data as CodePipeline.GetPipelineStateOutput[] });
+          this.setState({
+            isLoading: false,
+            codePipelines: res.data as CodePipeline.GetPipelineStateOutput[],
+          });
         })
         .catch((err) => {
           console.error('Error:', err);
@@ -39,7 +43,7 @@ class App extends React.Component<Props, State> {
     }
   };
 
-  public render() {
+  public render(): ReactNode {
     return (
       <div className="app">
         {this.state.isLoading && !this.state.codePipelines && (
@@ -48,61 +52,58 @@ class App extends React.Component<Props, State> {
             <h3>Loading</h3>
           </div>
         )}
-        {this.state.codePipelines &&
-          this.state.codePipelines.map(codePipeline => {
-            return (
+        {this.state.codePipelines
+          && this.state.codePipelines.map((codePipeline) => (
               <div key={codePipeline.pipelineName} className="codePipeline">
                 <h2 className="codePipeline__name">{codePipeline.pipelineName}</h2>
                 <ol className="stageStates">
-                  {codePipeline.stageStates &&
-                    codePipeline.stageStates.map((stageState, index) => {
-                      return (
+                  {codePipeline.stageStates
+                    && codePipeline.stageStates.map((stageState, index) => (
                         <li
                           key={stageState.stageName}
                           className={classNames('stageState', {
                             'stageState--error':
-                              stageState.latestExecution &&
-                              stageState.latestExecution.status === 'Failed',
+                              stageState.latestExecution
+                              && stageState.latestExecution.status === 'Failed',
                             'stageState--in-progress':
-                              stageState.latestExecution &&
-                              stageState.latestExecution.status === 'InProgress',
+                              stageState.latestExecution
+                              && stageState.latestExecution.status === 'InProgress',
                             'stageState--success':
-                              stageState.latestExecution &&
-                              stageState.latestExecution.status === 'Succeeded',
+                              stageState.latestExecution
+                              && stageState.latestExecution.status === 'Succeeded',
                           })}
                         >
                           <h3 className="stageState__name">
                             {index + 1}. {stageState.stageName}
                           </h3>
-                          {stageState.latestExecution &&
-                            stageState.latestExecution.status === 'InProgress' && (
+                          {stageState.latestExecution
+                            && stageState.latestExecution.status === 'InProgress' && (
                               <div className="in-progress-icon" />
-                            )}
-                          {stageState.latestExecution &&
-                            stageState.latestExecution.status === 'Failed' && (
+                          )}
+                          {stageState.latestExecution
+                            && stageState.latestExecution.status === 'Failed' && (
                               <img src={bug} alt="logo" className="error-icon" />
-                            )}
+                          )}
                           <ol className="actionStates">
-                            {stageState.actionStates &&
-                              stageState.actionStates.map(actionState => {
-                                return (
+                            {stageState.actionStates
+                              && stageState.actionStates.map((actionState) => (
                                   <li
                                     key={actionState.actionName}
                                     className={classNames('actionState', {
                                       'actionState--error':
-                                        actionState.latestExecution &&
-                                        actionState.latestExecution.status === 'Failed',
+                                        actionState.latestExecution
+                                        && actionState.latestExecution.status === 'Failed',
                                       'actionState--in-progress':
-                                        actionState.latestExecution &&
-                                        actionState.latestExecution.status === 'InProgress',
+                                        actionState.latestExecution
+                                        && actionState.latestExecution.status === 'InProgress',
                                       'actionState--success':
-                                        actionState.latestExecution &&
-                                        actionState.latestExecution.status === 'Succeeded',
+                                        actionState.latestExecution
+                                        && actionState.latestExecution.status === 'Succeeded',
                                     })}
                                   >
                                     {actionState.actionName}
-                                    {actionState.latestExecution &&
-                                      actionState.latestExecution.status === 'Failed' && (
+                                    {actionState.latestExecution
+                                      && actionState.latestExecution.status === 'Failed' && (
                                         <>
                                           <div className="actionState__error-description">
                                             {actionState.latestExecution.summary}
@@ -115,18 +116,15 @@ class App extends React.Component<Props, State> {
                                             </div>
                                           )}
                                         </>
-                                      )}
+                                    )}
                                   </li>
-                                );
-                              })}
+                              ))}
                           </ol>
                         </li>
-                      );
-                    })}
+                    ))}
                 </ol>
               </div>
-            );
-          })}
+          ))}
       </div>
     );
   }
