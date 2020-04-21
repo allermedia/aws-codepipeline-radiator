2869c910b21aaba42e55ac29b7a6d9021ba452df
Mikko Tikkanen <mikko.tikkanen@aller.com>
2020-04-21 16:40:12 +0300

    feat: switch SDK to serverside (#3)
    * switch aws-sdk to serverside
    - Seprate client and serverside
    - npm update
    
    * Added ability to get multiple codepipeline datasets
    
    * Fixed env variable for aws-sdk region
    
    * Added optional aws-sdk key secret env variables
    
    * cleanup
    
    * Removed .vscode
    
    * added .vscode to gitignore
    
    * cleanup
    
    * Fixed instructions
    
    * Removed default region
    
    * Added more error checing
    
    * Added common scripts and dependencies
    
    * Set tests running + ESLint fix
    
    * Updated react-scripts
    
    * eslint fixes
    
    * cleanup
    
    * cleanup
    
    * renamed env example
    
    * documentation
    
    * Added start script
    
    * documentation
    
    * npm update
    
    * fixed scripts
    
    * uninstalled unused dependency
    
    * Added dev documentation
    
    * Fixed configuration documentation
    
    * documentation fix

---
+++ server/src/routes/index.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import { CodePipeline } from 'aws-sdk';
+import { GetPipelineStateOutput } from 'aws-sdk/clients/codepipeline';
+
+const codepipeline = new CodePipeline();
+const router = Router();
+
+const getPipelineState = (name: string): Promise<GetPipelineStateOutput> => codepipeline
+  .getPipelineState({ name }).promise();
+
+router.get('/api/codepipelines/state', (req, res) => {
+  if (!req.query.q) {
+    res.status(400).json({
+      message: 'Query (q=) param is required',
+    });
+  } else {
+    const codePipelineNames = req.query.q.split(',');
+    Promise.all(codePipelineNames.map(getPipelineState))
+      .then((data) => res.json(data))
+      .catch((err) => {
+        console.log('Error:', err);
+        res.status(500).json({
+          message: err.message,
+        });
+      });
+  }
+});
+
+export default router;
