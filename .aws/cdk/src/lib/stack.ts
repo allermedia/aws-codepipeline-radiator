98995cab1ae742c434b8cacf17a8d7d79f535845
Mikko Tikkanen <mikko.tikkanen@aller.com>
2020-05-05 13:39:44 +0300

    added codepipeline principal to lambda

--- .aws/cdk/src/lib/stack.ts
+++ .aws/cdk/src/lib/stack.ts
@@ -1,6 +1,7 @@
 import { Stack, App } from '@aws-cdk/core';
 import { LambdaRestApi } from '@aws-cdk/aws-apigateway';
 import { Function, AssetCode, Runtime } from '@aws-cdk/aws-lambda';
+import { ServicePrincipal } from '@aws-cdk/aws-iam';
 
 export default class ApplicationStack extends Stack {
   constructor(scope: App, id: string) {
@@ -8,17 +9,21 @@ export default class ApplicationStack extends Stack {
 
     // domain: this.node.tryGetContext('domain'),
 
-    const handler = new Function(this, 'ApiFunction', {
+    const lambdaFunction = new Function(this, 'ApiFunction', {
       runtime: Runtime.NODEJS_12_X,
       code: AssetCode.asset('../../server'),
       handler: 'dist/bin/lambda.handler',
       environment: {
         // BUCKET: bucket.bucketName
       },
+      logRetention: 30,
+    });
+    lambdaFunction.addPermission('allowCodePipelineRead', {
+      principal: new ServicePrincipal('codepipeline.amazonaws.com'),
     });
 
     new LambdaRestApi(this, 'RestApi', {
-      handler,
+      handler: lambdaFunction,
     });
   }
 };
