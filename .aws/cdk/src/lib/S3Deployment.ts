8b2216cc372b0534d1ed6487b67df950a1776cdd
Mikko Tikkanen <mikko.tikkanen@aller.com>
2020-05-19 11:55:48 +0300

    Added cloudfront and certificate

--- .aws/cdk/src/lib/S3Deployment.ts
+++ .aws/cdk/src/lib/S3Deployment.ts
@@ -1,7 +1,9 @@
-import { App, CfnOutput, Construct } from '@aws-cdk/core';
+import { CfnOutput, Construct } from '@aws-cdk/core';
 import { Bucket } from '@aws-cdk/aws-s3';
 import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
-import { CloudFrontWebDistribution, OriginAccessIdentity } from '@aws-cdk/aws-cloudfront';
+import { CloudFrontWebDistribution, OriginAccessIdentity, ViewerCertificate } from '@aws-cdk/aws-cloudfront';
+import { Certificate, ValidationMethod, DnsValidatedCertificate } from '@aws-cdk/aws-certificatemanager';
+import { HostedZone } from '@aws-cdk/aws-route53';
 
 
 export type S3DeploymentProps = {
@@ -9,6 +11,16 @@ export type S3DeploymentProps = {
    * S3 deployment path
    */
   sourcePath: string;
+
+  /**
+   * List of domain names used for CloudFront CNAME and certificate
+   */
+  domainNames: string[];
+
+  /**
+   * Route53 hosted zone domain name for DNS certificate validation
+   */
+  hostedZoneDomainName: string;
 }
 
 /**
@@ -16,7 +28,9 @@ export type S3DeploymentProps = {
  */
 export class S3Deployment extends Construct {
   constructor(scope: Construct, id: string, {
-    sourcePath
+    sourcePath,
+    domainNames = [],
+    hostedZoneDomainName,
   }: S3DeploymentProps) {
     super(scope, id);
 
@@ -29,7 +43,31 @@ export class S3Deployment extends Construct {
     const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
     bucket.grantRead(originAccessIdentity);
 
-    // Create CloudFront distribution with S3 source and origin access identity
+    // Create certificate
+    let certificate: Certificate;
+    const [mainDomainName, ...alternativeDomainNames] = domainNames;
+    if (hostedZoneDomainName) {
+      // If we have route53 domain name, create DnsValidatedCertificate
+      const hostedzone = HostedZone.fromLookup(this, 'HostedZone', {
+        domainName: hostedZoneDomainName,
+      });
+      certificate = new DnsValidatedCertificate(this, 'Certificate', {
+        domainName: mainDomainName,
+        hostedZone: hostedzone,
+        subjectAlternativeNames: alternativeDomainNames,
+        validationMethod: ValidationMethod.DNS,
+      });
+
+    } else {
+      // Create default certificate that does email validation
+      certificate = new Certificate(this, 'Certificate', {
+        domainName: mainDomainName,
+        subjectAlternativeNames: alternativeDomainNames,
+        validationMethod: ValidationMethod.DNS,
+      });
+    }
+
+    // Create CloudFront distribution with S3 source, origin access identity and certificate
     const distribution = new CloudFrontWebDistribution(this, 'Distribution', {
       originConfigs: [{
         s3OriginSource: {
@@ -38,6 +76,9 @@ export class S3Deployment extends Construct {
         },
         behaviors: [{ isDefaultBehavior: true }],
       }],
+      viewerCertificate: ViewerCertificate.fromAcmCertificate(certificate, {
+        aliases: domainNames, // Need to redefine the aliases (https://github.com/aws/aws-cdk/issues/7240)
+      }),
     });
 
     // Create bucket deployment
@@ -49,6 +90,6 @@ export class S3Deployment extends Construct {
 
 
     new CfnOutput(this, 'S3 Bucket name', { value: bucket.bucketName });
-    new CfnOutput(this, 'CloudFront Distribution domain name', { value: distribution.domainName });
+    new CfnOutput(this, 'CloudFront Distribution ID', { value: distribution.distributionId });
   }
 };
